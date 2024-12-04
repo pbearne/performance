a92b899448df6fb5d1bbef3ed1204ac5db0e8593
Weston Ruter <westonruter@google.com>
2024-12-04 15:37:51 -0800

    Update test case to test for multiple sources

--- plugins/image-prioritizer/tests/test-cases/picture-element-as-lcp-tablet-and-desktop-metrics-missing.php
+++ plugins/image-prioritizer/tests/test-cases/picture-element-as-lcp-tablet-and-desktop-metrics-missing.php
@@ -23,7 +23,7 @@ return array(
 							'viewport_width' => $viewport_width,
 							'elements'       => array(
 								array(
-									'xpath' => '/*[1][self::HTML]/*[2][self::BODY]/*[1][self::PICTURE]/*[2][self::IMG]',
+									'xpath' => '/*[1][self::HTML]/*[2][self::BODY]/*[1][self::PICTURE]/*[3][self::IMG]',
 									'isLCP' => true,
 								),
 							),
@@ -42,6 +42,7 @@ return array(
 			<body>
 				<picture>
 					<source type="image/avif" srcset="https://example.com/foo-300x225.avif 300w, https://example.com/foo-1024x768.avif 1024w, https://example.com/foo-768x576.avif 768w, https://example.com/foo-1536x1152.avif 1536w, https://example.com/foo-2048x1536.avif 2048w" sizes="(max-width: 600px) 480px, 800px">
+					<source type="image/webp" srcset="https://example.com/foo-300x225.webp 300w, https://example.com/foo-1024x768.webp 1024w, https://example.com/foo-768x576.webp 768w, https://example.com/foo-1536x1152.webp 1536w, https://example.com/foo-2048x1536.webp 2048w" sizes="(max-width: 600px) 480px, 800px">
 					<img fetchpriority="high" decoding="async" width="1200" height="800" src="https://example.com/foo.jpg" alt="Foo" srcset="https://example.com/foo-300x225.jpg 300w, https://example.com/foo-1024x768.jpg 1024w, https://example.com/foo-768x576.jpg 768w, https://example.com/foo-1536x1152.jpg 1536w, https://example.com/foo-2048x1536.jpg 2048w" sizes="(max-width: 600px) 480px, 800px">
 				</picture>
 			</body>
@@ -57,7 +58,8 @@ return array(
 			<body>
 				<picture>
 					<source type="image/avif" srcset="https://example.com/foo-300x225.avif 300w, https://example.com/foo-1024x768.avif 1024w, https://example.com/foo-768x576.avif 768w, https://example.com/foo-1536x1152.avif 1536w, https://example.com/foo-2048x1536.avif 2048w" sizes="(max-width: 600px) 480px, 800px">
-					<img data-od-removed-fetchpriority="high" data-od-xpath="/*[1][self::HTML]/*[2][self::BODY]/*[1][self::PICTURE]/*[2][self::IMG]"  decoding="async" width="1200" height="800" src="https://example.com/foo.jpg" alt="Foo" srcset="https://example.com/foo-300x225.jpg 300w, https://example.com/foo-1024x768.jpg 1024w, https://example.com/foo-768x576.jpg 768w, https://example.com/foo-1536x1152.jpg 1536w, https://example.com/foo-2048x1536.jpg 2048w" sizes="(max-width: 600px) 480px, 800px">
+					<source type="image/webp" srcset="https://example.com/foo-300x225.webp 300w, https://example.com/foo-1024x768.webp 1024w, https://example.com/foo-768x576.webp 768w, https://example.com/foo-1536x1152.webp 1536w, https://example.com/foo-2048x1536.webp 2048w" sizes="(max-width: 600px) 480px, 800px">
+					<img data-od-removed-fetchpriority="high" data-od-xpath="/*[1][self::HTML]/*[2][self::BODY]/*[1][self::PICTURE]/*[3][self::IMG]"  decoding="async" width="1200" height="800" src="https://example.com/foo.jpg" alt="Foo" srcset="https://example.com/foo-300x225.jpg 300w, https://example.com/foo-1024x768.jpg 1024w, https://example.com/foo-768x576.jpg 768w, https://example.com/foo-1536x1152.jpg 1536w, https://example.com/foo-2048x1536.jpg 2048w" sizes="(max-width: 600px) 480px, 800px">
 				</picture>
 				<script type="module">/* import detect ... */</script>
 			</body>
